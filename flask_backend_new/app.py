cbae4e29ad6efef0b8cda7d384c1e7f9dadd4211
Chuanyu Pan <chuanyu_pan@berkeley.edu>
2025-02-15 12:04:33 +0800

    add a new flask backend

--- flask_backend_new/app.py
+++ flask_backend_new/app.py
@@ -1,13 +1,14 @@
+import os
 from pathlib import Path
 import json
 import copy
 from flask import Flask, request, jsonify
-from functions import parse_html, match, BS_fill_js
+from functions import parse_html, match, BS_fill_js, db_get_page_by_id
 
 app = Flask(__name__)
 working_dir = Path(__file__).parent
 CONFIG = json.load(open(working_dir / "config.json", "r", encoding="utf-8"))
-
+URL_HASH_FILE = working_dir / "cache" / "hash_url.json"
 
 # 执行学生数据匹配主函数
 def main(html: str, student_id: str):
@@ -17,6 +18,94 @@ def main(html: str, student_id: str):
     # 数据库字段与前端匹配
     matches, db_data = match(student_id=student_id, parse_result=parse_res, ai_api_key=CONFIG["ai_api_key"], db_api_key=CONFIG["db_api_key"])
     return matches, parse_out, db_data
+
+@app.route('/url', methods=['POST', "GET"])
+def url():
+    # TODO: 未引入多线程保护机制
+    # 保存大学申请网页对应的hash ID,格式为{hash ID: 学校申请页面的url}
+    hash_name = json.load(open(URL_HASH_FILE, "r", encoding="utf-8"))
+    if request.method == 'POST':
+        data = request.json
+        url_name = data.get('name')
+        identifier = data.get('hash_id')
+        if identifier not in hash_name.keys():
+            hash_name[identifier] = url_name
+            json.dump(hash_name, open(URL_HASH_FILE, "w", encoding="utf-8"), indent=4)
+            return jsonify({"message": "success"}), 200
+        else:
+            return jsonify({"message": "exist"}), 200
+        
+# 缓存方案：更新字段映射
+@app.route('/mappings', methods=['POST'])
+def mappings():
+    data = request.json
+    name = data.get('name')
+    mapping = data.get('mappings')
+    matching_file = working_dir / "cache" / f"cache_{name}" / "match.json"
+    # 更新字段映射
+    matches = json.load(open(matching_file, "r", encoding="utf-8"))
+    for Id, Key in mapping.items():
+        if Key is not []:
+            for field in matches:
+                if field["Id"] == Id:
+                    field["Key"] = Key
+                    break
+
+    json.dump(matches, open(matching_file, "w", encoding="utf-8"), indent=4)
+    return jsonify({"message": "Cache successfully updated"}), 200
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.json
+    name = data.get('name')  # 申请页面的hash ID
+    student_id = data.get('student_id')  # 学生ID
+    password = data.get('password')  # 管理员密码
+    html_source = data.get('html_source')  # 学生申请页面的html源码
+    
+    url_hash = json.load(open(URL_HASH_FILE, "r", encoding="utf-8"))
+    cache_dir = working_dir / "cache" / f"cache_{name}"
+    
+    if name in url_hash and cache_dir.exists(): # page has been cached before
+        with open(cache_dir / "parse.json", "r") as f:
+            parse_res = json.load(f)
+        with open(cache_dir / "match.json", "r") as f:
+            matches = json.load(f)
+        # load student data from db
+        db_data = db_get_page_by_id(
+            notion_secret_key=CONFIG["db_api_key"],
+            page_id=student_id
+        )
+    else: # page not cached in file
+        # create cache dir
+        os.makedirs(cache_dir, exist_ok=True)
+        # run parsing and matching
+        matches, parse_res, db_data = main(
+            html=html_source,
+            student_id=student_id
+        )
+        # save files
+        with open(cache_dir / "parse.json", "w") as f:
+            json.dump(parse_res, f)
+        with open(cache_dir / "match.json", "w") as f:
+            json.dump(matches, f)
+    
+    filler = BS_fill_js(
+        html=html_source,
+        html_parse=parse_res,
+        match_result=matches,
+        db_data=db_data
+    )
+    
+    fill_set = filler.Fill()
+
+    db_fields = []
+    for field in db_fields:
+        db_fields.append({
+            'value': field,
+            'label': field,
+        })
+    
+    return jsonify({"filled_set": fill_set, "parsed_fields": parse_res, "students_fields": db_fields})
 
 def simple_submit():
     test_html_file = "UW.html"
